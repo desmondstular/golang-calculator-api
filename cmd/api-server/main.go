d899aa084849c4eea1c26b7c0fa2a28f16275bd2
Desmond Stular <dstular@yahoo.com>
2025-03-13 22:09:51 -0600

    Created a decode handler to test decoding request payload

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
@@ -8,11 +9,19 @@ import (
 
 const Port = ":8080"
 
+type Numbers struct {
+    A int
+    B int
+}
+
 func main() {
     mux := http.NewServeMux()
 
     // Home handler
     mux.HandleFunc("GET /", homeHandler)
+
+    // Decode handler test
+    mux.HandleFunc("POST /decode", decodeHandler)
 
     fmt.Printf("Listening on port %s...\n", Port[1:])
     log.Fatal(http.ListenAndServe(":8080", mux))
@@ -21,4 +30,18 @@ func main() {
 func homeHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     fmt.Fprintf(w, "Path %s : received GET", r.URL.Path)
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+    var n Numbers
+
+    fmt.Printf("%+v\n", r.Body)
+
+    err := json.NewDecoder(r.Body).Decode(&n)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    fmt.Fprintf(w, "numbers: %+v\n", n)
 }
